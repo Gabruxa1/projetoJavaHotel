65029a9fde3370151626c7aae34d89febbe10a4e
Gabriel Cordeiro <gabriel.harik@gmail.com>
2023-06-05 19:20:57 -0300

    Finalização CRUD

---
+++ JavaHotel/src/main/java/com/mycompany/javahotel/JavaHotel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javahotel;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class JavaHotel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
